498c2c72e8eb9affb1d514a3bc63693e3f469cc8
alison5839 <127799253+alison5839@users.noreply.github.com>
2023-05-26 18:08:00 -0300

    Add files via upload

---
+++ Avaliacao-03/avaliacao-03.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    struct Funcionario{
+    int idade;
+    char nome[30];
+    float salario;
+};
+    struct Funcionario funcionario;
+
+    printf("Digite o nome do funcionário: ");
+    fgets(funcionario.nome, 30, stdin);
+  
+    printf("Digite a idade do funcionário: ");
+    scanf("%d", &funcionario.idade);
+  
+    printf("Digite o salário do funcionário: ");
+    scanf("%f", &funcionario.salario);
+
+    FILE *FPRT = fopen("funcionario.txt", "w");
+    if (FPRT == NULL) {
+        printf("Erro ao abrir o arquivo.");
+        return 1;
+    }
+ 
+    fprintf(FPRT, "Nome: %s\n", funcionario.nome);
+    fprintf(FPRT, "Idade: %d\n", funcionario.idade);
+    fprintf(FPRT, "Salário: %.2f\n", funcionario.salario);
+
+    fclose(FPRT);
+
+    printf("Dados gravados com sucesso em 'funcionario.txt'.\n");
+
+    return 0;
+}
